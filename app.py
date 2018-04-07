db5697700abb97f311410de9f321f41153d05b95
mridulganga <mridul.kepler@gmail.com>
2018-04-07 23:08:49 +0530

    added layout and nav

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/layout')
+def layout_page():
+    return render_template('layout.html')
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8000, debug=True)
