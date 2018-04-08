d85451cb8d59a56aeb06e09a518af1331880aaf8
mridulganga <mridul.kepler@gmail.com>
2018-04-08 14:33:49 +0530

    database

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, g, render_template, flash, redirect, url_for, abort
 from flask_bcrypt import check_password_hash
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from db import *
 
 app = Flask(__name__)
 
@@ -16,7 +17,45 @@ def layout_page():
 
 @app.route('/login')
 def login_page():
+    result = user_login(email, password):
+    if result:
+        session["login"] = True
+        session["user_id"] = result[0]
+        session["username"] = result[1]
+        session["email"] = result[3]
     return render_template('login.html')
+
+
+@app.route('/members')
+def member_page():
+    conn = db.connectDB()
+    query = ("SELECT username,email,bio,ieee_no,branch,sem FROM members")
+    cursor = connection.cursor()
+    cursor.execute(query)
+
+    for (username,email,bio,ieee_no,branch,sem) in cursor:
+	       print('{} {} {} {} {} {}'.format(username,email,bio,ieee_no,branch,sem))
+
+    connection.close()
+    return render_template('members.html')
+
+
+@app.route('/addpost',methods=["GET","POST"])
+def add_post():
+    if request.method=='POST':
+        c=connectDB()
+        id="69"
+        words= request.form["addpost"]
+        ts = time.time()
+        timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+        query="insert into posts (user_id,article,date_time) values ({},{},{})".format(id,words,timestamp)
+        executeDB(c,query)
+        disconnectDB()
+
+        return "Posted"
+    else:
+        return render_template('addpost.html')
+    return render_template('addpost.html')
 
 
 # @app.route('/logout')
--- db.py
+++ db.py
@@ -1,14 +1,14 @@
 import mysql.connector
-
+from flask_bcrypt import generate_password_hash, check_password_hash
 def connectDB(host='192.168.0.100',database='codejam',user='root',password='1234'):
     return mysql.connector.connect(host=host,database=database,user=user,password=password)
 
 def disconnectDB(conn):
     conn.close()
 
-def executeDB(conn,sql):
+def executeDB(conn,sql,values):
     cursor = conn.cursor()
-    cursor.execute(sql)
+    cursor.execute(sql,values)
     conn.commit()
 
 def queryDB(conn,sql):
@@ -19,26 +19,120 @@ def queryDB(conn,sql):
     return rows
 
 
-def user_register():
-    pass
+def user_register(username,password,email,bio,dob,ieee_no,branch,sem):
+    c = connectDB()
+    password = generate_password_hash(password)
+    executeDB(c,"insert into members values(0,%s,%s,%s,%s,%s,%s,%s,%s)",(username,password,email,bio,dob,ieee_no,branch,sem))
+    disconnectDB(c)
+    return True
 
-def user_login():
-    pass
+def user_login(email,password):
+    c = connectDB()
+    result = queryDB(c,"select * from members where email='"+email+"'")
+    disconnectDB(c)
+    if check_password_hash(result[0][2], password):
+        return result[0]
+    else:
+        return False
+# Tests
+# user_register("mridulganga","mridul1698","mridul.kepler@gmail.com","i am pro","1998-01-16","94082800","ise","6")
+# user_login("mridul.kepler@gmail.com","i")
 
-def like_post():
-    pass
+def like_post(user_id,post_id):
+    c = connectDB()
+    executeDB(c,"insert into likes values("+user_id+","+post_id+")",())
+    disconnectDB(c)
+    return True
 
-def unlike_post():
-    pass
+# like_post('1','2')
 
-def add_post():
-    pass
+def unlike_post(user_id,post_id):
+    c = connectDB()
+    executeDB(c,"delete from likes where user_id="+user_id+" and post_id="+post_id,())
+    disconnectDB(c)
+    return True
 
-def remove_post():
-    pass
+# unlike_post('1','2')
 
-def edit_post():
-    pass
+def add_post(user_id,post_data,group):
+    c = connectDB()
+    executeDB(c,"insert into posts values("+user_id+","+group+",0,%s,now())",(post_data,))
+    disconnectDB(c)
+    return True
+
+# add_post('1','hello','0')
+
+def remove_post(post_id):
+    c = connectDB()
+    executeDB(c,"delete from posts where post_id="+post_id,())
+    disconnectDB(c)
+    return True
+
+# remove_post('1')
+
+def edit_post(article, post_id):
+    c = connectDB()
+    executeDB(c,"update posts set article='"+article+"' where post_id="+post_id,())
+    disconnectDB(c)
+    return True
+
+# edit_post('Hello!!!!!', '2')
+def follow(from_id,to_id):
+    c = connectDB()
+    executeDB(c,"insert into follow values("+from_id+","+to_id+")",())
+    disconnectDB(c)
+    return True
+
+# follow('1','2')
+# follow('3', '2')
+# follow('4', '2')
+
+def unfollow(from_id,to_id):
+    c = connectDB()
+    executeDB(c,"delete from follow where from_id="+from_id + " and to_id="+to_id,())
+    disconnectDB(c)
+    return True
+
+# unfollow('1','2')
+# unfollow('3', '2')
+# unfollow('4', '2')
+
+def post_list():
+    c = connectDB()
+    result = queryDB(c,"select * from posts where group=0")
+    disconnectDB(c)
+    return result
+
+
+def group_post_list(group):
+    c = connectDB()
+    result = queryDB(c,"select * from posts where group="+group)
+    disconnectDB(c)
+    return result
+
+# print post_list()
+
+def follower_list(user_id):
+    c = connectDB()
+    result = queryDB(c,"select username from members where id in (select from_id from follow where to_id="+user_id+")")
+    disconnectDB(c)
+    return result
+
+def member_info(user_id):
+    c = connectDB()
+    result = queryDB(c,"select * from members where id="+user_id)
+    disconnectDB(c)
+    return result
+
+def members_list():
+    c = connectDB()
+    result = queryDB(c,"select user_id,username,email,branch,sem from members")
+    disconnectDB(c)
+    return result
+
+#print members_list()
+
+# print follower_list('2')
 
 # Tables
 # members
